d7a6a5876169eab5737b0e647c915bcd83835b48
gustin <gustin@users.noreply.github.com>
2019-12-16 21:28:20 -0500

    Handle authenticate start return values and finalize -- hurrah!

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,13 @@ use bincode::{deserialize, serialize};
 use curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
 use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
 use curve25519_dalek::scalar::Scalar;
-use ed25519_dalek::Keypair;
+use ed25519_dalek::{Keypair, Signature};
 use futures::{future, Future};
 use hkdf::Hkdf;
 use hmac::{Hmac, Mac};
 use js_sys::{Promise, JSON};
 use opaque::*;
+use opaque::sigma::KeyExchange;
 use rand_os::OsRng;
 use serde::{Deserialize, Serialize};
 use sha2::Sha512;
@@ -41,7 +42,7 @@ pub struct AuthData {
     pub beta: [u8; 32],
     pub v: [u8; 32],
     pub envelope: Vec<u8>,
-    pub key: [u8; 32],
+    pub key: Vec<u8>,
     pub y: [u8; 32]
 }
 
@@ -236,6 +237,7 @@ pub async fn registration_init(username: String, password: String) {
     );
     opts.body(Some(&JsValue::from_str(&body)));
 
+    log!("====> Authentication Start");
     let request = Request::new_with_str_and_init(
         "http://localhost:8000/authenticate/start",
         &opts,
@@ -260,11 +262,157 @@ pub async fn registration_init(username: String, password: String) {
     log!("Key Exchange 2: {:?}", result.key);
     log!("Y: {:?}", result.y);
 
+    // ==> Authenticate Final!!!
+    let beta_point = CompressedRistretto::from_slice(&result.beta[..]);
+    let beta_a = beta_point.decompress().unwrap();
+
+    let v_point = CompressedRistretto::from_slice(&result.v[..]);
+    let v_a = v_point.decompress().unwrap();
+
+    let y_point = CompressedRistretto::from_slice(&result.y[..]);
+    let y = y_point.decompress().unwrap();
+
+    // OPRF
+
+    log!("-) beta {:?}:", beta_a);
+    log!("-) v {:?}:", v_a);
+    log!("-) AuthEnv {:?}:", result.envelope);
+
+    let inverse_r_a = r_a.invert();
+    let sub_beta_a = beta_a * inverse_r_a;
+
+    log!("-) {{1/r}} {:?}:", inverse_r_a);
+    log!("-) beta^{{1/r}} {:?}:", sub_beta_a);
+
+    log!("*) RwdU = H(x, v, beta^{{1/r}})");
+
+    let mut hasher_a = Sha3_512::new();
+    hasher_a.input(password.as_bytes()); // NOTE: Harden with a key derivitive, Section 3.4
+    hasher_a.input(v_a.compress().to_bytes());
+    hasher_a.input(sub_beta_a.compress().to_bytes());
+    let rwd_u_a = hasher_a.result();
+
+    log!("-) RwdU {:?}:", rwd_u_a);
+
+    // Use rwd_u_a to decrypt envelope
+
+    let hkdf_a = Hkdf::<Sha512>::new(None, &rwd_u_a);
+    let mut okm_a = [0u8; 44]; // 32 byte key + 96 bit nonce
+    let info_a = hex::decode("f0f1f2f3f4f5f6f7f8f9").unwrap(); // make info the domain string, +
+    hkdf_a.expand(&info_a, &mut okm_a).unwrap();
+
+    log!("-) HKDF OKM {}", hex::encode(&okm_a[..]));
+
+    let encryption_key_a: GenericArray<u8, typenum::U32> =
+        GenericArray::clone_from_slice(&okm_a[0..32]);
+    let aead = Aes256GcmSiv::new(encryption_key_a);
+    let nonce_a: GenericArray<u8, typenum::U12> =
+        GenericArray::clone_from_slice(&okm_a[32..44]);
+
+    log!("-) encryption key 32-byte {:?}:", encryption_key_a);
+    log!("-) nonce 96 bit {:?}:", nonce_a);
 
 
+    let envelope_decrypted = aead
+        .decrypt(&nonce, result.envelope.as_slice())
+        .expect("decryption failure");
+    let envelope_for_realz: Envelope =
+        bincode::deserialize(envelope_decrypted.as_slice()).unwrap();
+
+    log!("=) EnvU (decoded) {:?}:", envelope_for_realz);
+
+    // SIGMA
+
+    //  KE3 = Sig(PrivU; g^y, g^x), Mac(Km2; IdU)
+    // { A, SIGa(g^y, g^x), MAC(Km; A) } Ke
+
+    // decrypt ke_2
+    let dh: RistrettoPoint = x * y;
+    log!("-) Shared Secret: {:?}", dh);
+
+    let hkdf = Hkdf::<Sha512>::new(None, dh.compress().as_bytes());
+    let mut okm_dh = [0u8; 108]; // 32 byte key, 96 bit nonce, 64 bytes
+    let info = hex::decode("f0f1f2f3f4f5f6f7f8f9").unwrap();
+    hkdf.expand(&info, &mut okm_dh).unwrap();
+
+    let encryption_key_dh: GenericArray<u8, typenum::U32> =
+        GenericArray::clone_from_slice(&okm_dh[0..32]);
+    let aead_dh = Aes256GcmSiv::new(encryption_key_dh);
+    let nonce_dh: GenericArray<u8, typenum::U12> =
+        GenericArray::clone_from_slice(&okm_dh[32..44]);
+
+    log!("-) DH encryption key 32-byte {:?}:", encryption_key_dh);
+    log!("-) DH nonce 96 bit {:?}:", nonce_dh);
+
+    log!("* Decrypting: {:?}", result.key);
+    // Guard: verify HMAC on B
+    log!("* Slice: {:?}", result.key.as_slice());
+
+    let key_2_decrypted = aead_dh
+        .decrypt(&nonce_dh, result.key.as_slice())
+        .expect("decryption failure");
+    let key_2_for_realz: KeyExchange =
+        bincode::deserialize(key_2_decrypted.as_slice()).unwrap();
+
+    // SIGa(g^y, g^x)
+    let mut prehashed: Sha3_512 = Sha3_512::new();
+    prehashed.input(y.compress().as_bytes());
+    prehashed.input(ke_1);
+    let context: &[u8] = b"SpecificCustomerDomainName";
+    let sig: Signature = keypair.sign_prehashed(prehashed, Some(context));
+
+    // MAC(Km; PubS)
+    let mut mac = HmacSha512::new_varkey(&okm_dh[44..108]).unwrap();
+    mac.input(&pub_u);
+
+    let key_exchange_3 = KeyExchange {
+        identity: pub_u,
+        signature: &sig.to_bytes(),
+        mac: mac.result().code().as_slice().to_vec(),
+    };
+
+    let payload_dh: Vec<u8> = bincode::serialize(&key_exchange_3).unwrap();
+    let encrypted_ke_3 =
+        aead_dh.encrypt(&nonce_dh, payload_dh.as_slice()).unwrap();
+
+    // sidA
+    // sidB
+    // { infoA, A, sigA(nB, sidA, g^x, infoA, infoA), MAC kM(A)} Ke
 
     //    let (beta_a, v_a, envelope_a) =
      //       authenticate_2(username, &alpha_a);
+    let mut opts = RequestInit::new();
+    opts.method("POST");
+    opts.mode(RequestMode::Cors);
+    let body = format!(
+        r#"
+        {{
+            "username": "{}",
+            "key": {:?},
+            "x": {:?}
+        }}
+        "#,
+        username, encrypted_ke_3, ke_1
+    );
+    opts.body(Some(&JsValue::from_str(&body)));
+
+    let request = Request::new_with_str_and_init(
+        "http://localhost:8000/authenticate/login",
+        &opts,
+    )
+    .unwrap();
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request))
+        .await
+        .unwrap();
+
+    let resp: Response = resp_value.dyn_into().unwrap();
+    let json = JsFuture::from(resp.json().unwrap()).await.unwrap();
+
+    let j_string = JSON::stringify(&json).unwrap();
+    log!("{:?}", j_string.as_string().unwrap());
+
 
 }
 
